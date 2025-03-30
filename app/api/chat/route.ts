8daee84ef5f59060617fea1164c83845d5919e4a
Sarthak vaish <sarthakvaish184@gmail.com>
2025-03-30 17:44:19 +0530

    fix

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,7 +2,7 @@ import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow responses up to 5 minutes
-export const maxDuration = 300;
+export const maxDuration = 59;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
